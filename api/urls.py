98063f46ad2b727d28fdb07834b1ae5ab594ce23
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-06-03 13:49:08 +0300

    store services

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products", views.api_products),
-    path("products/<str:pk>", views.api_product),
-    path("categories", views.api_categories),
-    path("categories/<str:pk>", views.api_category)
+    path("products", views.APIProductsView.as_view(), name="api_products"),
+    path("products/<str:pk>", views.APIProductView.as_view(), name="api_product"),
+    path("categories", views.APICategoriesView.as_view(), name="api_categories"),
+    path("categories/<str:pk>", views.APICategoryView.as_view(), name="api_category"),
 ]
