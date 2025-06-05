152199c3db69cc7a4179039f449d0cab6aee23c6
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-06-05 12:15:59 +0300

    created cart

--- storeapp/models.py
+++ storeapp/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
     title = models.CharField(max_length=200)
     category_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, unique=True)
     slug = models.SlugField(default= None)
-    featured_product = models.OneToOneField('Product', on_delete=models.CASCADE, blank=True, null=True, related_name='featured_product')
+    featured_product = models.ForeignKey('Product', on_delete=models.CASCADE, blank=True, null=True, related_name='featured_product')
     icon = models.CharField(max_length=100, default=None, blank = True, null=True)
 
     def __str__(self):
